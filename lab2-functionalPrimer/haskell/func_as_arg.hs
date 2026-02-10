69bb6e436466821655ad2386f8ec42aa4eb41e94
HarveyChadwick <240326227@aston.ac.uk>
2026-02-10 13:20:32 +0000

    Completed Task 1,2,3

--- lab2-functionalPrimer/haskell/func_as_arg.hs
+++ lab2-functionalPrimer/haskell/func_as_arg.hs
@@ -1,9 +1,12 @@
---create inpFunc
-inpFunc = [1..5] 
-
---Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
-
-main = do
-    let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
-    putStrLn("sum = " ++ show(result))
+#A more general version of func_as_arg.py that works for a set of Ints between a and b
+ 
+def listFunc(a, b): 
+  return [i for i in range(a, b+1)] #Create list of ints from 1 to 5, Haskell equivalent [1..5]
+ 
+def applicatorFunc(inpFunc, s, a, b):
+  if s=='s': 
+    return sum(inpFunc(a, b)) 
+  else: 
+    return sum(inpFunc(a, b))/(b-a+1)
+ 
+print(applicatorFunc(listFunc, 'a', 1, 10))
